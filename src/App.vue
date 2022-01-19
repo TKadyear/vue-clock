bd4306cc7d711e31e51af5e0c65adac9edf72f8d
TKadyear <tamarakadyear@gmail.com>
2022-01-19 22:03:27 +0100

    Basic config DigitalClock

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DigitalClock from "./components/DigitalClock.vue"
+</script>
+
+<template>
+  <DigitalClock />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/DigitalClock.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+  name: "DigitalClock",
+  data() {
+    return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  },
+  mounted() {
+    setInterval(() => this.setTime(), 1000)
+  },
+  methods: {
+    setTime() {
+      const date = new Date();
+      this.hours = date.getHours();
+      this.minutes = date.getMinutes();
+      this.seconds = date.getSeconds();
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Digital Clock</h1>
+  <div class="container">
+    <div class="LCD">
+      <div class="hours">{{ hours }}</div>
+      <div class="divider">:</div>
+      <div class="minutes">{{ minutes }}</div>
+      <div class="divider">:</div>
+      <div class="seconds">{{ seconds }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.LCD {
+  display: flex;
+}
+</style>
